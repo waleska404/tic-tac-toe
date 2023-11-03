577f2eebd70a5f39dc2b31981bbe4310ba4f8604
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-03 16:55:34 +0100

    :construction: create new game on real time data base

--- app/src/main/java/com/waleska404/tictactoe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/waleska404/tictactoe/ui/home/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.waleska404.tictactoe.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.waleska404.tictactoe.data.network.FirebaseService
+import com.waleska404.tictactoe.data.network.model.GameData
+import com.waleska404.tictactoe.data.network.model.PlayerData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -10,8 +12,19 @@ class HomeViewModel @Inject constructor(
     private val firebaseService: FirebaseService
 ) : ViewModel() {
     fun onCreateGame() {
+        firebaseService.createGame(createNewGame())
     }
 
     fun onJoinGame(id: String) {
+    }
+
+    private fun createNewGame(): GameData {
+        val currentPlayer = PlayerData(playerType = 1)
+        return GameData(
+            board = List(9) { 0 },
+            player1 = currentPlayer,
+            player2 = null,
+            playerTurn = currentPlayer
+        )
     }
 }
