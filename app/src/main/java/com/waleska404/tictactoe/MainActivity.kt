e003eccaa0a2197d28cc127755bfd88e039a2e60
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-07 20:57:33 +0100

    :lipstick: home screen design

--- app/src/main/java/com/waleska404/tictactoe/MainActivity.kt
+++ app/src/main/java/com/waleska404/tictactoe/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
             TicTacToeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     navigationController = rememberNavController()
                     ContentWrapper(navigationController = navigationController)
--- app/src/main/java/com/waleska404/tictactoe/ui/home/HomeScreen.kt
+++ app/src/main/java/com/waleska404/tictactoe/ui/home/HomeScreen.kt
@@ -1,14 +1,28 @@
 package com.waleska404.tictactoe.ui.home
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,30 +30,109 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.waleska404.tictactoe.R
+import com.waleska404.tictactoe.ui.theme.Background
+import com.waleska404.tictactoe.ui.theme.LightGrey
+import com.waleska404.tictactoe.ui.theme.PrimaryBlack
+import com.waleska404.tictactoe.ui.theme.PrimaryGrey
+import com.waleska404.tictactoe.ui.theme.SecondaryGrey
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
     navigateToGame: (String, String, Boolean) -> Unit,
 ) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = Modifier.weight(2f))
-        CreateGame(
-            onCreateGame = { homeViewModel.onCreateGame(navigateToGame) }
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Divider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(2.dp)
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        JoinGame(
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Background)
+    ) {
+        Header()
+        Body(
+            onCreateGame = { homeViewModel.onCreateGame(navigateToGame) },
             onJoinGame = { gameId -> homeViewModel.onJoinGame(gameId, navigateToGame) }
         )
-        Spacer(modifier = Modifier.weight(2f))
+    }
+}
+
+@Composable
+fun Header() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.height(24.dp))
+        // logo
+        Box(
+            modifier = Modifier
+                .size(224.dp)
+                .padding(12.dp)
+                .clip(RoundedCornerShape(24.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "tic tac toe app logo",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(24.dp)
+            )
+        }
+
+        // title
+        Text(
+            text = "Tic Tac Toe",
+            fontSize = 32.sp,
+            color = PrimaryBlack,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun Body(
+    onCreateGame: () -> Unit,
+    onJoinGame: (String) -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(24.dp),
+        colors = cardColors(
+            containerColor = Background,
+        ),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            var createGame by remember { mutableStateOf(true) }
+            Switch(
+                checked = createGame,
+                onCheckedChange = { createGame = it },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = SecondaryGrey,
+                    checkedTrackColor = PrimaryGrey,
+                    uncheckedThumbColor = SecondaryGrey,
+                    uncheckedTrackColor = PrimaryGrey,
+                    uncheckedBorderColor = Color.Transparent
+                ),
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            AnimatedContent(targetState = createGame, label = "") {
+                when(it) {
+                    true -> CreateGame(onCreateGame)
+                    false -> JoinGame(onJoinGame)
+                }
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+        }
     }
 }
 
@@ -47,8 +140,17 @@ fun HomeScreen(
 fun CreateGame(
     onCreateGame: () -> Unit,
 ) {
-    Button(onClick = { onCreateGame() }) {
-        Text(text = "Create game")
+    Button(
+        onClick = { onCreateGame() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = PrimaryGrey,
+        ),
+    ) {
+        Text(
+            text = "Create Game",
+            color = PrimaryBlack,
+            fontSize = 18.sp
+        )
     }
 }
 
@@ -59,12 +161,34 @@ fun JoinGame(
 ) {
     var text by remember { mutableStateOf("") }
 
-    TextField(value = text, onValueChange = { text = it })
-
-    Button(
-        onClick = { onJoinGame(text) },
-        enabled = text.isNotEmpty(),
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Text(text = "Join game")
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = PrimaryBlack,
+                cursorColor = PrimaryBlack,
+                focusedIndicatorColor = SecondaryGrey,
+                unfocusedIndicatorColor = PrimaryGrey,
+                containerColor = LightGrey
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { onJoinGame(text) },
+            enabled = text.isNotEmpty(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = PrimaryGrey
+            )
+        ) {
+            Text(
+                text = "Join Game",
+                color = PrimaryBlack,
+                fontSize = 18.sp
+            )
+        }
     }
 }
